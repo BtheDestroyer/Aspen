5bc0c1e657f942961ebd8584a49afe324d15f68d
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2019-03-22 05:21:22 -0700

    V0.1 release documentation

--- inc/Debug.hpp
+++ inc/Debug.hpp
@@ -10,24 +10,51 @@ namespace Aspen
 /// \brief Debug namespace
 namespace Debug
 {
+/// \brief Debugger wrapper class for ImGui
 class Debug : public Object::Object
 {
+  /// \brief ImGuiIO
   ImGuiIO *_io;
+  /// \brief Total number of Debugger Objects
   static unsigned _dcount;
+  /// \brief Objects to close in the Object tree
   std::vector<const Object *> _toClose;
+  /// \brief Objects to open in the Object tree
   std::vector<const Object *> _toOpen;
 
-public:
-  Debug(Object *parent = nullptr, std::string name = "Debug");
+  /// \brief Sets up ImGui
   void Setup();
-  void operator()();
-  void MakeTree(Object *o);
+
+public:
+  /// \brief Constructor
+  ///        Derived classes should call this in their constructors' initialization list
+  /// \param parent Parent Object to be passed to Object constructor
+  /// \param name Object name
+  ///             Set by derived classes to a string representation of their type
+  Debug(Object *parent = nullptr, std::string name = "Debug");
+  /// \brief Destructor
   ~Debug();
 
+  /// \brief Updates this object and all of its children
+  ///        Derived classes should call or reimplement this at some point in their operator()
+  ///        This won't run if the Object isn't Active
+  void operator()();
+
+  /// \brief Creates the Object tree for the given object
+  /// \param o Object to the debugger tree for
+  void MakeTree(Object *o);
+
+  /// \brief Closes every tree node in the Object tree
   void CloseAll();
+  /// \brief Closes the tree node in the Object tree for the given Object
+  /// \param o Object of which the tree node will be closed
   void Close(Object *o);
+  /// \brief Opens the tree node in the Object tree for the given Object
+  /// \param o Object of which the tree node will be opened
   void Open(Object *o);
 
+  /// \brief Fills out the Debugger if it exists with this Object's information
+  ///        Derived classes should call their base class's version of this method
   void PopulateDebugger();
 };
 } // namespace Debug
