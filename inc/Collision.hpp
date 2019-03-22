57ef93f201025c0303b4913c4b3ad492f9f75f19
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2019-03-22 04:17:40 -0700

    V0.1 release documentation

--- inc/Collision.hpp
+++ inc/Collision.hpp
@@ -7,27 +7,41 @@ namespace Aspen
 /// \brief Physics namespace
 namespace Physics
 {
+/// \brief Potential collision result values
 enum COLLISION_RESULT
 {
+  /// \brief No collision occurred
   FAILURE,
+  /// \brief A collision occurred
   SUCCESS,
+  /// \brief The collider couldn't detect collision with the other Object
   CANNOT_HANDLE
 };
 
+/// \brief Forward declaration
 class Collider;
 
+/// \brief Collision class
 class Collision
 {
 public:
+  /// \brief Result of the tested collision
+  ///        This class should be discarded unless this is SUCCESS
   COLLISION_RESULT result;
+  /// \brief Second collider in a collision
   Collider *collider;
+  /// \brief Relative x position of the collision
   double collisionX;
+  /// \brief Relative y position of the collision
   double collisionY;
+  /// \brief Angle the collision occurred at
   double collisionAngle;
 
+  /// \brief Constructor
+  /// \param other Second collider in the collision
   explicit Collision(Collider *other);
 };
-} // namespace Collision
+} // namespace Physics
 } // namespace Aspen
 
 #endif
